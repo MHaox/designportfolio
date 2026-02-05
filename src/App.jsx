d8c78d864171bc588d6c9e51e9e9e74141349e65
MHAOX <max.hoogeweg@outlook.com>
2026-02-05 11:08:44 +0100

    Presure cooker v.1.1.01

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PostItBoard from './PortfolioV1.2.1';
 import OldPortfolio from './PortfolioV1.1.30';
+import Receipts from './PressureCookerV1.1.01';
 
 
   export default function App() {
@@ -14,6 +15,9 @@ import OldPortfolio from './PortfolioV1.1.30';
         
         {/* The "/progress" path loads your OLD design */}
         <Route path="/progress" element={<OldPortfolio />} />
+
+        {/* The "/Fast Fasion" path loads the result of the pressure cooker assignmnet*/}
+        <Route path="/pressure-cooker" element={<Receipts />} />
       </Routes>
     </BrowserRouter>
   );
