9beb6f3b890ee0a7fe55b3f008302a692493c1d4
MHAOX <max.hoogeweg@outlook.com>
2026-04-01 00:13:39 +0200

    Add Vercel Analytics integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 // src/App.jsx
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import PortfolioV1_1 from './pages/PortfolioV1.1';
 import PortfolioV1_2 from './pages/PortfolioV1.2'; 
-  //Sub-Project V1.2
-  import Receipts from './pages/PressureCookerV1';
-  import ResourceHub from './pages/ResourceHub';
+import Receipts from './pages/PressureCookerV1';
+import ResourceHub from './pages/ResourceHub';
 import PortfolioV1_3 from './pages/PortfolioV1.3';
 
-  export default function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PortfolioV1_2 />} />
-        
-        <Route path="/V1.1" element={<PortfolioV1_1 />} />
-        <Route path="/V1.2" element={<PortfolioV1_2 />} />
-        <Route path="/V1.3" element={<PortfolioV1_3 />} />
-        
-        <Route path="/pressure-cooker" element={<Receipts />} />
-        <Route path="/resource-hub" element={<ResourceHub />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PortfolioV1_2 />} />
+          <Route path="/V1.1" element={<PortfolioV1_1 />} />
+          <Route path="/V1.2" element={<PortfolioV1_2 />} />
+          <Route path="/V1.3" element={<PortfolioV1_3 />} />
+          <Route path="/pressure-cooker" element={<Receipts />} />
+          <Route path="/resource-hub" element={<ResourceHub />} />
+        </Routes>
+      </BrowserRouter>
+      
+      {/* Place the Analytics component here */}
+      <Analytics />
+    </>
   );
 }
